eef4de9ad8585e35a73ecf03de10ceb24a6e62a5
justKrow <krowxen2214@gmail.com>
2024-10-30 06:44:05 +0000

    surfaces

--- asteroid_shooter/blank_window.py
+++ asteroid_shooter/blank_window.py
@@ -6,6 +6,10 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 pygame.display.set_caption('asteroid shooter')
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+# create a surface
+test_surface = pygame.Surface((400, 100))
+# we need to attach the surface to the display surface
+
 
 while True:  # run forever -> keeps our game running
     # Do not run the code until i tell you
@@ -17,7 +21,10 @@ while True:  # run forever -> keeps our game running
             sys.exit()
 
     # 2. updates
-    # nothing yet :)
+    # draw the surface on the display surface
+    display_surface.fill((95, 191, 167))
+    test_surface.fill((182, 62, 111))
+    display_surface.blit(test_surface, (0, 0))
 
     # 3. show the frame to the player / update the display surface
     pygame.display.update()
