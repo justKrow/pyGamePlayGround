3851a10532f0e2e1e61d382c2e7bb461a865b57e
justKrow <krowxen2214@gmail.com>
2024-12-06 04:19:20 +0000

    laser logic

--- asteroid_shooter/code/app.py
+++ asteroid_shooter/code/app.py
@@ -1,6 +1,14 @@
 import pygame
 import sys
 
+
+def laser_update(laser_list, speed):
+    for laser in laser_list:
+        laser.y -= speed * dt
+        if laser.bottom < 0:
+            laser_list.remove(laser)
+
+
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 pygame.display.set_caption('asteroid shooter')
@@ -14,7 +22,7 @@ ship_surf = pygame.image.load(
 ship_rect = ship_surf.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2))
 
 laser_surf = pygame.image.load('../graphics/laser.png').convert_alpha()
-laser_rect = laser_surf.get_rect(midbottom=(ship_rect.midtop))
+laser_list = []
 
 # importing background image
 background_surf = pygame.image.load(
@@ -27,6 +35,9 @@ text_rect = text_surf.get_rect(midbottom=(
     WINDOW_WIDTH // 2, WINDOW_HEIGHT - 80))
 test_surface = pygame.Surface((200, 100))
 
+# drawing
+test_rect = pygame.Rect(500, 100, 50, 50)
+
 while True:
     # event loop
     for event in pygame.event.get():
@@ -34,17 +45,28 @@ while True:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('shoot laser')
+            laser_rect = laser_surf.get_rect(midbottom=(ship_rect.midtop))
+            laser_list.append(laser_rect)
+
     # framerate limit
-    clock.tick(120)  # limit the frame rate to 60 frames per second
-    # ship_rect.center = pygame.mouse.get_pos()
+    dt = clock.tick(120) / 1000  # limit the frame rate to 60 frames per second
+    ship_rect.center = pygame.mouse.get_pos()
 
     # 2. updates
-    laser_rect.y -= 3
+    laser_update(laser_list, 300)
     display_surface.fill((0, 0, 0))
     display_surface.blit(background_surf, (0, 0))
-    display_surface.blit(ship_surf, ship_rect)
     display_surface.blit(text_surf, text_rect)
-    display_surface.blit(laser_surf, laser_rect)
+
+    # rect drawing
+    pygame.draw.rect(display_surface, "white",
+                     text_rect.inflate(30, 30), width=4, border_radius=10)
+    display_surface.blit(ship_surf, ship_rect)
+
+    for laser in laser_list:
+        display_surface.blit(laser_surf, laser)
 
     # 3. show the frame to the player / update the display surface
     pygame.display.update()
