3bbbb68ead1996812541ea12d4ba419a18fb0486
justKrow <krowxen2214@gmail.com>
2024-12-15 23:06:37 +0000

    laser

--- asteroid_object_files/code/app.py
+++ asteroid_object_files/code/app.py
@@ -28,6 +28,7 @@ class Ship(pygame.sprite.Sprite):
         if pygame.mouse.get_pressed()[0] and self.can_shoot:
             self.can_shoot = False
             self.shoot_time = pygame.time.get_ticks()
+            Laser(laser_group, self.rect.midtop)
 
     def update(self):
         self.move()
@@ -41,9 +42,17 @@ class Laser(pygame.sprite.Sprite):
         self.image = pygame.image.load('../graphics/laser.png').convert_alpha()
         self.rect = self.image.get_rect(
             midbottom=pos)
+        # float based position
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(0, -1)
+        self.speed = 600
+
+    def update(self):
+        self.pos += self.direction * self.speed * dt
+        self.rect.topleft = (
+            round(self.pos.x), round(self.pos.y))  # basic setup
 
 
-# basic setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -60,7 +69,6 @@ laser_group = pygame.sprite.Group()
 
 # sprite creation
 ship = Ship(spaceship_group)
-laser = Laser(laser_group, (500, 100))
 
 # game loop
 while True:
@@ -76,6 +84,7 @@ while True:
     display_surface.blit(background_surface, (0, 0))
 
     spaceship_group.update()
+    laser_group.update()
 
     # graphics
     spaceship_group.draw(display_surface)
